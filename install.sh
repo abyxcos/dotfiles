da05fab5083033ba12e40bbcffa707dcbf4ab3b4
root <root@lap-tur-grey.a-bb.net>
2011-08-29 18:22:02 -0400

    basic support for relinking (.bin => bin)

--- install.sh
+++ install.sh
@@ -7,9 +7,19 @@ git submodule update
 # Check if files need to be replaced
 for f in *
 do
-	if [ `grep $f .ignore` ]
-	then
+	if [ `grep $f .ignore` ]; then
 		echo "Ignoring $f"
+	elif [ "`grep ^$f .relink`" ]; then
+		g=`sed -r -n 's/^'$f' ~~ (.*)/\1/p' < .relink`
+		echo "Linking \".$f\" to \"~/$g\""
+		if [ -e ~/$g ]
+		then
+			echo "Replacing \"~/$g\""
+			diff ~/$g $f
+			rm ~/$g
+		fi
+
+		ln -s `pwd`/$f ~/$g
 	else
 		if [ -e ~/.$f ]
 		then
